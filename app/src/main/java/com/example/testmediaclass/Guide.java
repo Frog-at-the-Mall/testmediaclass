f5df59055164783affc1159f77f47f51d0203a8b
mattb <mattb.frye@gmail.com>
2022-01-18 15:55:26 -0500

    Initial commit

---
+++ app/src/main/java/com/example/testmediaclass/Guide.java
@@ -0,0 +1,15 @@
+package com.example.testmediaclass;
+
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.AudioAttributes;
+import android.media.MediaPlayer;
+import android.provider.MediaStore;
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Guide {
+
+}
---
+++ app/src/main/java/com/example/testmediaclass/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.testmediaclass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.Image;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+
+//is activity && implements SensorListeners for compass i think
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+
+    private SensorManager SensorManage;
+
+    private ImageView compassImage;
+
+    private float DegreeStart = 0f;
+
+    TextView DegreeTV;
+
+    MediaPlayer player;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        compassImage = (ImageView) findViewById(R.id.image);
+        DegreeTV = (TextView) findViewById(R.id.DegreeTV);
+
+        // initialize your android device sensor capabilities
+        SensorManage = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        player = MediaPlayer.create(this, R.raw.sink_into_the_underground);
+        player.start();
+
+
+
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // to stop the listener and save battery
+        SensorManage.unregisterListener(this);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // code for system's orientation sensor registered listeners
+        SensorManage.registerListener(this, SensorManage.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+                SensorManager.SENSOR_DELAY_GAME);
+    }
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // get angle around the z-axis rotated
+        float degree = Math.round(event.values[0]);
+        DegreeTV.setText("Heading: " + Float.toString(degree) + " degrees");
+
+        //connecting mediaplayer to degrees from north
+        player.setVolume(degree/100,degree/100);
+        // rotation animation - reverse turn degree degrees
+        RotateAnimation ra = new RotateAnimation(
+                DegreeStart,
+                -degree,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+        // set the compass animation after the end of the reservation status
+        ra.setFillAfter(true);
+        // set how long the animation for the compass image will take place
+        ra.setDuration(210);
+        // Start animation of compass image
+        compassImage.startAnimation(ra);
+        DegreeStart = -degree;
+    }
+
+
+
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
